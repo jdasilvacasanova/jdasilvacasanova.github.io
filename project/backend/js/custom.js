b88e39540df4430d5092975ef05bdea942ccd21b
Joao Casanova <joao.casanova@gmail.com>
2016-11-25 01:10:30 +0000

    Fixing Menu...

--- project/backend/js/custom.js
+++ project/backend/js/custom.js
@@ -28,9 +28,9 @@ var getUrlParameter = function getUrlParameter(sParam) {
 };
 
 var Produtos = [
-{ID:"1",Nome:"Bitoque", Página:"Carne",Preço:"11",Descrição:"Batatas Fritas, arroz, bife de vaca, salada, ovo estrelado", Imagem: "resources/bitoque.jpg", Activo:"true"},
-{ID:"2",Nome:"Mousse de chocolate", Página:"Sobremesa",Preço:"2",Descrição:"Mousse de chocolate", Imagem: "resources/mousse.jpg", Activo:"false"},
-{ID:"3",Nome:"Bacalhau á Braz", Página:"Peixe",Preço:"13",Descrição:"Bacalhau, etc...", Imagem: "resources/bacalhau.jpg", Activo:"true"}];
+{ID:"1",Nome:"Bitoque", Página:"Carne",Preço:"11",Descrição:"Batatas Fritas, arroz, bife de vaca, salada, ovo estrelado", Imagem: "resources/bitoque.jpg", Activo:true},
+{ID:"2",Nome:"Mousse de chocolate", Página:"Sobremesas",Preço:"2",Descrição:"Mousse de chocolate", Imagem: "resources/mousse.jpg", Activo:false},
+{ID:"3",Nome:"Bacalhau á Braz", Página:"Peixe",Preço:"13",Descrição:"Bacalhau, etc...", Imagem: "resources/bacalhau.jpg", Activo:true}];
 
 if(localStorage.getItem('produtos')==null){
 	localStorage.setItem('produtos',JSON.stringify(Produtos));
@@ -40,8 +40,8 @@ else{
 }
 
 var Utilizadores = [
-{ID:"1",Nome:"John Smith", Nivel:"Administrador",Email:"JohnSmith@restaurant.com",Password:"Password", Imagem: "resources/JohnSmith.jpg",Activo:"true"},
-{ID:"2",Nome:"John Cook", Nivel:"Empregado",Email:"JohnCook@restaurant.com",Password:"Password", Imagem: "resources/JohnCook.jpg",Activo:"true"}];
+{ID:"1",Nome:"John Smith", Nivel:"Administrador",Email:"JohnSmith@restaurant.com",Password:"Password", Imagem: "resources/JohnSmith.jpg",Activo:true},
+{ID:"2",Nome:"John Cook", Nivel:"Empregado",Email:"JohnCook@restaurant.com",Password:"Password", Imagem: "resources/JohnCook.jpg",Activo:true}];
 
 if(localStorage.getItem('utilizadores')==null){
 	localStorage.setItem('utilizadores',JSON.stringify(Utilizadores));
@@ -51,9 +51,9 @@ else{
 }
 
 var Mesas = [
-{ID:"1",Nome:"Mesa 1", Tipo:"Balcao",Capacidade:"1",Activo:"true"},
-{ID:"2",Nome:"Mesa 2", Tipo:"Balcao",Capacidade:"1",Activo:"true"},
-{ID:"3",Nome:"Mesa 3", Tipo:"Mesa",Capacidade:"4",Activo:"true"}];
+{ID:"1",Nome:"Mesa 1", Tipo:"Balcao",Capacidade:"1",Activo:true},
+{ID:"2",Nome:"Mesa 2", Tipo:"Balcao",Capacidade:"1",Activo:true},
+{ID:"3",Nome:"Mesa 3", Tipo:"Mesa",Capacidade:"4",Activo:true}];
 
 if(localStorage.getItem('mesas')==null){
 	localStorage.setItem('mesas',JSON.stringify(Mesas));
@@ -63,11 +63,11 @@ else{
 }
 
 var Paginas = [
-{ID:"1",Nome:"Inicio", SubPaginaDe:"",Ordem:"1",Imagem:"resources/Inicio.jpg",Activo:"true"},
-{ID:"2",Nome:"Carne", SubPaginaDe:"Inicio",Ordem:"1",Imagem:"resources/Carnes.jpg",Activo:"true"},
-{ID:"3",Nome:"Peixe", SubPaginaDe:"Inicio",Ordem:"2",Imagem:"resources/Peixe.jpg",Activo:"true"},
-{ID:"4",Nome:"Sobremesas", SubPaginaDe:"Inicio",Ordem:"3",Imagem:"resources/Sobremesas.jpg",Activo:"true"},
-{ID:"5",Nome:"Entradas", SubPaginaDe:"Inicio",Ordem:"4",Imagem:"resources/Entradas.jpg",Activo:"true"},
+{ID:"1",Nome:"Inicio", SubPaginaDe:"",Ordem:"1",Imagem:"resources/Inicio.jpg",Activo:true},
+{ID:"2",Nome:"Carne", SubPaginaDe:"Inicio",Ordem:"1",Imagem:"resources/Carnes.jpg",Activo:true},
+{ID:"3",Nome:"Peixe", SubPaginaDe:"Inicio",Ordem:"2",Imagem:"resources/Peixe.jpg",Activo:true},
+{ID:"4",Nome:"Sobremesas", SubPaginaDe:"Inicio",Ordem:"3",Imagem:"resources/Sobremesas.jpg",Activo:true},
+{ID:"5",Nome:"Entradas", SubPaginaDe:"Inicio",Ordem:"4",Imagem:"resources/Entradas.jpg",Activo:true},
 ];
 
 if(localStorage.getItem('paginas')==null){
